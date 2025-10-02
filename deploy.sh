7b060abd1228bbc29f041d3eb4d212208ccee44d
bxx-114514 <tzhong072@gmail.com>
2025-10-03 03:38:47 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 2、如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:bxx-114514/bxx-114514.github.io.git gh-pages
+git push -f https://github.com/bxx-114514/bxx-114514.github.io.git gh-pages
 
 # 3、如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
